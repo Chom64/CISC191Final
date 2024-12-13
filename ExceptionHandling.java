5ae1ca53e783f2cd73378aa35073f6a8fbd612ce
Chom64 <141713120+Chom64@users.noreply.github.com>
2024-12-13 13:29:00 -0800

    Delete ExceptionHandling.java

--- ExceptionHandling.java
+++
@@ -1,6 +0,0 @@
-public class ExceptionHandling {
-    public static void handle(Exception e) {
-        // Handle exceptions and provide user feedback
-        System.err.println("An error occurred: " + e.getMessage());
-    }
-}
